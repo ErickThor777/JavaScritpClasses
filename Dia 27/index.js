336a189446ab75ee3a23a3ba535e2435746e73ea
Daniel Canqui <you.nightmare.00@gmail.com>
2021-10-13 09:48:56 -0400

    Add price.

--- Dia 27/index.js
+++ Dia 27/index.js
@@ -1,4 +1,10 @@
-const precios = [23, 235, 454, 4545, 4545];
+let precios = [23, 235, 454, 4545, 4545];
 
 console.log(precios[2]);
+
+precios = precios.map(precio => precio * 2);
+console.log(precios);
+
+precios.forEach((precio, index) => precios[index] = precio * 2);
+console.log(precios);
 
