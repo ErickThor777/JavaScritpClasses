934825b0f3c5dbc4d411b707d1e07796068f427b
Daniel Canqui <you.nightmare.00@gmail.com>
2021-10-15 10:07:48 -0400

    New  homework

---
+++ Dia 28/index.js
@@ -0,0 +1,11 @@
+// Crear un arreglo libros donde el elmento es un objeto que tiene precio y nombre debe tener 5 objetos minimos
+
+
+
+
+
+
+// ordernar el arreglo de libros por nombre
+
+
+// ordernar el arreglo de libros por precio
