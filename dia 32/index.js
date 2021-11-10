7d46987bcd15bb21ba3af6bc4ede7a654d962431
Daniel Canqui <you.nightmare.00@gmail.com>
2021-11-10 10:12:17 -0400

    Resolucion del ejercicio

--- dia 32/index.js
+++ dia 32/index.js
@@ -1,6 +1,13 @@
 let pedidos = ['churasco', 'charque', 'lomito', 'silpancho'];
+console.log(pedidos);
 
 // Añadir a cada pedido un precio quetodos cuesten 25
 
-pedidos = 
+pedidos = pedidos.map(elem => {
+  return {
+    name: elem,
+    precio: 25
+  };
+});
 
+console.log(pedidos);
