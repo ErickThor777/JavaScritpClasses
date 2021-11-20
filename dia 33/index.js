1fedc61f00f39c497270775123ab5d4296bed8ae
Daniel Canqui <you.nightmare.00@gmail.com>
2021-11-19 21:14:36 -0400

    repaso

---
+++ dia 33/index.js
@@ -0,0 +1,13 @@
+// ordenar el array
+const numero = ['daniel', 'canqui', 'erick', 'quinteros', 'abi', 'sergio'];
+
+
+
+// añadir con una sola linea un nuevo nombre al array 'ramiro'
+
+
+// covertir los elementos del array en un objeto con atributo name y edad igual 10
+
+
+// obener la suma de caracteres de todos los nombres del array
+
