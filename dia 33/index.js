7cf762636fa2366fc36398916e07be3510caabb4
Daniel Canqui <you.nightmare.00@gmail.com>
2021-11-19 22:11:28 -0400

    Resolucion del test

--- dia 33/index.js
+++ dia 33/index.js
@@ -1,13 +1,22 @@
 // ordenar el array
-const numero = ['daniel', 'canqui', 'erick', 'quinteros', 'abi', 'sergio'];
-
+let personas = ['daniel', 'canqui', 'erick', 'quinteros', 'abi', 'sergio'];
+personas.sort();
+console.log(personas);
 
 
 // añadir con una sola linea un nuevo nombre al array 'ramiro'
-
+personas.push('ramiro');
+console.log(personas);
 
 // covertir los elementos del array en un objeto con atributo name y edad igual 10
-
-
+personas = personas.map(name => {
+  return {
+    name: name,
+    edad: 10
+  };
+})
+console.log(personas);
 // obener la suma de caracteres de todos los nombres del array
+const totalCaracteres = personas.reduce((prev, next) => prev + next.name.length, 0);
+console.log(totalCaracteres);
 
