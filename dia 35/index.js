4c13afbdc64403ef5ecc702aa1d6108cbe3f41fe
Erick <ericktorricolopez@gmail.com>
2021-11-24 20:57:15 -0400

    nuevoavance0135

--- dia 35/index.js
+++ dia 35/index.js
@@ -17,5 +17,14 @@
 
 // filtrar los numeros igual a 4 y luego sumarlos +2  a cada uno
 let numeros = [4, 3, 2, 4, 2];
-let filtrar = numeros.filter((valor) => valor === 4).map((valor) => valor + 2);
+/* let filtrar = numeros.filter((valor) => valor === 4).map((valor) => valor + 2);
+console.log(filtrar); 
+ */
+let filtrar = numeros.filter((valor) => {
+    return valor === 4;
+}).map((valor) => {
+    return valor + 2;
+}).reduce((prev, nex) => {
+    return prev * nex;
+}, 1);
 console.log(filtrar);
