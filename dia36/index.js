3fff0d58d65b5f1725280683a3a0f7051ca0fd77
Daniel Canqui <you.nightmare.00@gmail.com>
2021-12-02 20:35:07 -0400

    New class

--- dia36/index.js
+++ dia36/index.js
@@ -1,1 +1,13 @@
-// crear una clase que tenga los atributos nombre y apellido y que tenga metodos que me devileva el nombre completo t pueda cambiar su nombre o apellido
+// crear una clase que tenga los atributos nombre y apellido y
+// que tenga metodos que me devileva el nombre completo, t pueda 
+//cambiar su nombre o apellido
+
+const Persona = require('./src/persona');
+const persona = new Persona('Adriana', 'Campos');
+console.log(persona);
+// persona.changeName('Daniel');
+persona.changeApellido('Canqui');
+console.log(persona);
+console.log(persona.getNombreCompleto());
+persona.nombre = 'Daniel';
+console.log(persona);
